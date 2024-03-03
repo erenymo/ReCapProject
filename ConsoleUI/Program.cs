49c5fabca13a56650c1efa6b41bd01e3c70915f1
erenymo <erenbalta342115@gmail.com>
2024-03-03 17:34:34 +0100

    Database Reviewed

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,10 +9,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        // TODO  
-        // ID veritabanında kendiliğinden eklenecek!!!
+        
         CarManager carManager = new CarManager(new EfCarDal());
-        Car car1 = new Car{Id = 38, BrandId = 1, ColorId = 5, DailyPrice = 100, ModelYear = 2014, Description = "Toyota Auris"};
+        Car car1 = new Car{BrandId = 1, ColorId = 5, DailyPrice = 100, ModelYear = 2014, Description = "Toyota Auris"};
         carManager.AddCar(car1);
         
         foreach (var car in carManager.GetCarsByBrandId(1))
